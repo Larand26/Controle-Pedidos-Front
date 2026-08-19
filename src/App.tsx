7a76b4587a1ca0734c8bff83b5096e17dbebaa50
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-19 12:19:54 -0300

    feat: cria rotas para as páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Finish from "./page/Finish";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Finish />} />
+
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/page/Finish.tsx
@@ -0,0 +1,7 @@
+export default function Finish() {
+  return (
+    <div className="text-3xl font-bold">
+      <h1>Finish</h1>
+    </div>
+  );
+}
